7534623e9a7caea46777b09df820450e4b70ff74
Prince tyagi <85237685+PrinceTyagia@users.noreply.github.com>
2025-04-26 23:54:48 +0530

    Update auth.controller.ts

--- auth-service/src/controllers/auth.controller.ts
+++ auth-service/src/controllers/auth.controller.ts
@@ -133,7 +133,7 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
       };
 
       // Set the authentication cookies
-      // setAuthCookies({ accessToken, refreshToken, user: userData }, res);
+      setAuthCookies({ accessToken, refreshToken, user: userData }, res);
 
       return res.send({
         success: true,
