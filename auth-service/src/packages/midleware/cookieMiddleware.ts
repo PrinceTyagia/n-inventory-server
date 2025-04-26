b86ffe91a54998b3eb80e0b0a5bcd73809011a56
Prince tyagi <85237685+PrinceTyagia@users.noreply.github.com>
2025-04-27 02:12:39 +0530

    Update cookieMiddleware.ts

--- auth-service/src/packages/midleware/cookieMiddleware.ts
+++ auth-service/src/packages/midleware/cookieMiddleware.ts
@@ -18,11 +18,10 @@ const setAuthCookies = ({ accessToken, refreshToken, user }: IUserCookies, res: 
   // Set cookie options
   const cookieOptions: CookieOptions = {
    httpOnly: true,
-  secure: true, // only use true in production
-  sameSite: 'none', // 'none' if cross-site with HTTPS
-    domain: '.vercel.app'
-  // secure: false, // only use true in production
-  // sameSite: 'lax', // 'none' if cross-site with HTTPS
+  secure: true, 
+  sameSite: 'strict',
+    path:'/'
+ 
   };
 
   // Set cookies with maxAge for expiration
